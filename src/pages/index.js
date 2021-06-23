39b79035e0fea33b0d9345abb993d0281bae7ba3
Junaid-Rasheed <junaidrasheed300@gmail.com>
2021-06-23 10:22:04 +0500

    theme-ui added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,25 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { Container, Heading, Button, Flex } from "theme-ui"
+import netlifyIdentity from "netlify-identity-widget"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Index() {
+  useEffect(() => {
+    netlifyIdentity.init({})
+  })
+
+  return (
+    <Container>
+      <Flex sx={{ flexDirection: "column", padding: 3 }}>
+        <Heading as="h1"> TODO APP</Heading>
+        <Button
+          sx={{ marginTop: 3, color: "black" }}
+          onClick={() => {
+            alert("hello")
+          }}
+        >
+          Log In
+        </Button>
+      </Flex>
+    </Container>
+  )
 }
